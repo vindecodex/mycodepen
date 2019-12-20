5292ba5ad95840e069f917266b5db6fc60ce8632
Vincent Villaluna <villalunavincent@gmail.com>
2019-12-20 10:14:29 +0800

    improve functionality

--- progress-bar-with-mouse-movement/index.js
+++ progress-bar-with-mouse-movement/index.js
@@ -6,11 +6,10 @@ let sp = document.querySelector("span")
 
 rating.addEventListener('mousemove',function(e){
   // mousemove e contains x and y of the mouse from the element
-  let val = ((e.pageX / rating.offsetWidth) * 100) - ratingRect.left
-  // updated val formula to allow using different width
+  let val = ((e.pageX - ratingRect.left) / rating.offsetWidth) * 100  //subtract it with element postion left/x to avoid mouse gap
   val = val > 100 ? 100 : val // must not be more than 100
   val = val < 20 ? 20 : val // must not be less than 20
   rate.style.width = val + "%"
   rate.setAttribute("rates",val) // we set custom attribute
-  sp.innerHTML = (val * .05).toFixed(1) // multiply it by .05 if we want perfect score will be 5 with decimal places of 1 as we set on toFixed function
+  sp.innerHTML = (val * .05).toFixed(1) // multiply it by .05 if we want perfect score will be 5 with decimal places of 1 as we set on toFixed functionp.innerHTML = val
 })
